037f181117b36b8b807a602f16926ffdd99ad984
Wallaby <avocado34.131@gmail.com>
2024-06-11 00:45:36 +0900

    feat(User) 프로필용 PreviewSupport 추가

---
+++ Projects/Feature/User/PreviewSupport/Sources/MockUpdateUserProfileUseCase.swift
@@ -0,0 +1,52 @@
+//
+//  MockUpdateUserProfileUseCase.swift
+//  PreviewSupportUser
+//
+//  Created by 이승기 on 6/10/24.
+//  Copyright © 2024 cakk. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+import DomainUser
+
+public struct MockUpdateUserProfileUseCase: UpdateUserProfileUseCase {
+  
+  // MARK: - Properties
+  
+  private let delay: TimeInterval
+  
+  private let scenario: Scenario
+  public enum Scenario {
+    case success
+    case failure
+  }
+  
+  
+  // MARK: - Initializers
+  
+  public init(
+    delay: TimeInterval = 1,
+    scenario: Scenario = .success
+  ) {
+    self.delay = delay
+    self.scenario = scenario
+  }
+  
+  
+  // MARK: - Public Methods
+  
+  public func execute(newUserProfile: DomainUser.NewUserProfile) -> AnyPublisher<Void, DomainUser.UserProfileError> {
+    switch scenario {
+    case .success:
+      Just(Void())
+        .delay(for: .seconds(delay), scheduler: RunLoop.main)
+        .setFailureType(to: UserProfileError.self)
+        .eraseToAnyPublisher()
+    case .failure:
+      Fail(error: UserProfileError.failure)
+        .eraseToAnyPublisher()
+    }
+  }
+}
