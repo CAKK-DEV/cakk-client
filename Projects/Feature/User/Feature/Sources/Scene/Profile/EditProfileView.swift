e59fed5c03a6261cd80e7721cc64c0ea811c31a3
Wallaby <avocado34.131@gmail.com>
2024-06-11 16:10:41 +0900

    rename(SwiftUIUtil) Optional value binding 유틸로 이동

--- Projects/Feature/User/Feature/Sources/Scene/Profile/EditProfileView.swift
+++ Projects/Feature/User/Feature/Sources/Scene/Profile/EditProfileView.swift
@@ -236,11 +236,3 @@ import PreviewSupportUser
   }
   return EditProfileView()
 }
-
-
-func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
-  Binding(
-    get: { lhs.wrappedValue ?? rhs },
-    set: { lhs.wrappedValue = $0 }
-  )
-}
