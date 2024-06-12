758a175f3a0741e3029cd312562ea651c7f61c01
Wallaby <avocado34.131@gmail.com>
2024-06-13 02:30:37 +0900

    feat(DomainCakeShop) 케이크샵 디테일 repository 추가

---
+++ Projects/Core/Domain/CakeShop/Sources/Repository/CakeShopDetailRepository.swift
@@ -0,0 +1,14 @@
+//
+//  CakeShopDetailRepository.swift
+//  DomainCakeShop
+//
+//  Created by 이승기 on 6/12/24.
+//  Copyright © 2024 cakk. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+public protocol CakeShopDetailRepository {
+  func fetch(shopId: Int) -> AnyPublisher<CakeShopDetail, CakeShopDetailError>
+}
