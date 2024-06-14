357a6f0f331b5308b268c0ac9a8f58bdc2c4fb62
Wallaby <avocado34.131@gmail.com>
2024-06-14 15:18:54 +0900

    fix(User) 잘못된 파라미터명 수정

--- Projects/Core/Domain/User/Sources/Entity/NewUserProfile.swift
+++ Projects/Core/Domain/User/Sources/Entity/NewUserProfile.swift
@@ -10,20 +10,20 @@ import Foundation
 
 public struct NewUserProfile {
   public var profileImageUrl: String?
-  public var nickName: String
+  public var nickname: String
   public var email: String
   public var gender: Gender
   public var birthday: Date?
   
   public init(
     profileImageUrl: String? = nil,
-    nickName: String,
+    nickname: String,
     email: String,
     gender: Gender,
     birthday: Date? = nil
   ) {
     self.profileImageUrl = profileImageUrl
-    self.nickName = nickName
+    self.nickname = nickname
     self.email = email
     self.gender = gender
     self.birthday = birthday
