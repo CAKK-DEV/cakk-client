ce5021a62a74b62d4bfb1af9633bda3a3073a68a
Wallaby <avocado34.131@gmail.com>
2024-06-16 03:40:10 +0900

    feat(NetworkSearch) 케이크 이미지 검색 repository 구현

---
+++ Projects/Core/Data/Network/Search/Sources/DTO/CakeImagesResponseDTO.swift
@@ -0,0 +1,21 @@
+//
+//  CakeImagesResponseDTO.swift
+//  NetworkSearch
+//
+//  Created by 이승기 on 6/16/24.
+//  Copyright © 2024 cakk. All rights reserved.
+//
+
+import Foundation
+
+struct CakeImagesResponseDTO: Decodable {
+  let returnCode: String
+  let returnMessage: String
+  let data: Data
+  
+  struct Data: Decodable {
+    let cakeImages: [CakeImageDTO]
+    let lastCakeId: Int
+    let size: Int
+  }
+}
