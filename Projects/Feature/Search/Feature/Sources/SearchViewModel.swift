479594dec46141ef087871d1d143c9052a2d3039
Wallaby <avocado34.131@gmail.com>
2024-06-15 16:18:22 +0900

    feat(Search) 인기 검색어 뷰 구현
    - SearchView 추가
    - SearchViewModel 추가

---
+++ Projects/Feature/Search/Feature/Sources/SearchViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  SearchViewModel.swift
+//  FeatureSearch
+//
+//  Created by 이승기 on 6/15/24.
+//  Copyright © 2024 cakk. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+import DomainSearch
+
+public final class SearchViewModel: ObservableObject {
+  
+  // MARK: - Properties
+  
+  @Published private(set) var trendingSearchKeywords: [String] = []
+  private let trendingSearchKeywordUseCase: TrendingSearchKeywordUseCase
+  
+  private var cancellables = Set<AnyCancellable>()
+  
+  
+  // MARK: - Initializers
+  
+  public init(trendingSearchKeywordUseCase: TrendingSearchKeywordUseCase) {
+    self.trendingSearchKeywordUseCase = trendingSearchKeywordUseCase
+  }
+  
+  
+  // MARK: - Public Methods
+  
+  public func fetchTrendingSearchKeywords() {
+    trendingSearchKeywordUseCase
+      .execute()
+      .subscribe(on: DispatchQueue.global())
+      .receive(on: DispatchQueue.main)
+      .sink { completion in
+        print(completion)
+      } receiveValue: { [weak self] keywords in
+        self?.trendingSearchKeywords = keywords
+      }
+      .store(in: &cancellables)
+  }
+  
+  
+  // MARK: - Private Methods
+}
