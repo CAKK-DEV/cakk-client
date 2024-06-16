cb949961a027d78db2dbd5d308317ea7042b36de
Wallaby <avocado34.131@gmail.com>
2024-06-17 08:23:31 +0900

    feat(Search) 케이크샵 검색 도메인 구현

---
+++ Projects/Core/Domain/Search/Sources/Entity/CakeShop.swift
@@ -0,0 +1,34 @@
+//
+//  CakeShop.swift
+//  DomainSearch
+//
+//  Created by 이승기 on 6/16/24.
+//  Copyright © 2024 cakk. All rights reserved.
+//
+
+import Foundation
+
+public struct CakeShop {
+  public let shopId: Int
+  public let profileImageUrl: String
+  public let name: String
+  public let bio: String
+  public let cakeImageUrls: [String]
+  public let workingDaysWithTime: [WorkingDayWithTime]
+  
+  public init(
+    shopId: Int,
+    profileImageUrl: String,
+    name: String,
+    bio: String,
+    cakeImageUrls: [String],
+    workingDaysWithTime: [WorkingDayWithTime]
+  ) {
+    self.shopId = shopId
+    self.profileImageUrl = profileImageUrl
+    self.name = name
+    self.bio = bio
+    self.cakeImageUrls = cakeImageUrls
+    self.workingDaysWithTime = workingDaysWithTime
+  }
+}
---
+++ Projects/Core/Domain/Search/Sources/UseCase/SearchCakeShopUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  SearchCakeShopUseCase.swift
+//  DomainSearch
+//
+//  Created by 이승기 on 6/16/24.
+//  Copyright © 2024 cakk. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+public protocol SearchCakeShopUseCase {
+  func execute(keyword: String?, latitude: Double, longitude: Double, pageSize: Int, lastCakeShopId: Int?) -> AnyPublisher<[CakeShop], Error>
+}
