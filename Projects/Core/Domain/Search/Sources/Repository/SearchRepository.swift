0f5112287a5f4968c7064861a460e7237831ce22
Wallaby <avocado34.131@gmail.com>
2024-06-20 16:50:02 +0900

    feat(Search) 지도 검색 도메인 추가

--- Projects/Core/Domain/Search/Sources/Repository/SearchRepository.swift
+++ Projects/Core/Domain/Search/Sources/Repository/SearchRepository.swift
@@ -13,4 +13,5 @@ public protocol SearchRepository {
   func fetchTrendingSearchKeywords(count: Int) -> AnyPublisher<[String], Error>
   func fetchCakeImages(keyword: String?, latitude: Double, longitude: Double, pageSize: Int, lastCakeId: Int?) -> AnyPublisher<[CakeImage], Error>
   func fetchCakeShops(keyword: String?, latitude: Double, longitude: Double, pageSize: Int, lastCakeShopId: Int?) -> AnyPublisher<[CakeShop], Error>
+  func fetchLocatedCakeShops(latitude: Double, longitude: Double) -> AnyPublisher<[LocatedCakeShop], Error>
 }
---
+++ Projects/Core/Domain/Search/Sources/UseCase/SearchCakeShopsOnMapUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  SearchLocatedCakeShopUseCase.swift
+//  DomainSearch
+//
+//  Created by 이승기 on 6/17/24.
+//  Copyright © 2024 cakk. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+public protocol SearchLocatedCakeShopUseCase {
+  func execute(longitude: Double, latitude: Double) -> AnyPublisher<[LocatedCakeShop], Error>
+}
