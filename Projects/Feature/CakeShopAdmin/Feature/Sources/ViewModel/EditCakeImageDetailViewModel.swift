c5309285682f497c193da47ec4190375e6b6e983
Wallaby <avocado34.131@gmail.com>
2024-07-01 23:12:53 +0900

    feat(CakeShopAdmin) 케이크 이미지 수정 구현

--- Projects/Feature/CakeShopAdmin/Feature/Sources/ViewModel/EditCakeImageDetailViewModel.swift
+++ Projects/Feature/CakeShopAdmin/Feature/Sources/ViewModel/EditCakeImageDetailViewModel.swift
@@ -18,6 +18,9 @@ public final class EditCakeImageDetailViewModel: ObservableObject {
   private let cakeImageId: Int
   @Published var tagString = ""
   
+  @Published private(set) var categories: [CakeCategory] = []
+  @Published private(set) var tags: [String] = []
+  
   private let cakeImageDetailUseCase: CakeImageDetailUseCase
   @Published private(set) var cakeImageDetail: CakeImageDetail?
   @Published private(set) var cakeImageDetailFetchingState: CakeImageDetailFetchingState = .idle
@@ -77,16 +80,45 @@ public final class EditCakeImageDetailViewModel: ObservableObject {
         }
       } receiveValue: { [weak self] cakeImageDetail in
         self?.cakeImageDetail = cakeImageDetail
+        self?.categories = cakeImageDetail.categories
+        self?.tags = cakeImageDetail.tags
       }
       .store(in: &cancellables)
   }
   
   public func updateCakeImage() {
+    guard let cakeImageDetail else { return }
     
+    cakeImageUpdatingState = .loading
+    
+    editCakeImageUseCase.execute(cakeImageId: cakeImageId,
+                                 imageUrl: cakeImageDetail.imageUrl,
+                                 categories: categories,
+                                 tags: tags)
+    .sink { [weak self] completion in
+      if case .failure(let error) = completion {
+        self?.cakeImageUpdatingState = .failure
+        print(error.localizedDescription)
+      } else {
+        self?.cakeImageUpdatingState = .success
+      }
+    } receiveValue: { _ in }
+    .store(in: &cancellables)
   }
   
   public func deleteCakeImage() {
+    cakeImageDeletingState = .loading
     
+    deleteCakeImageUseCase.execute(cakeImageId: cakeImageId)
+      .sink { [weak self] completion in
+        if case .failure(let error) = completion {
+          self?.cakeImageDeletingState = .failure
+          print(error.localizedDescription)
+        } else {
+          self?.cakeImageDeletingState = .success
+        }
+      } receiveValue: { _ in }
+      .store(in: &cancellables)
   }
   
   public func addNewTag(tagString: String) {
@@ -118,5 +150,16 @@ public final class EditCakeImageDetailViewModel: ObservableObject {
     } else {
       cakeImageDetail?.categories.append(category)
     }
+  }
+  
+  public func hasChanges() -> Bool {
+    guard let cakeImageDetail else { return false }
+    
+    if cakeImageDetail.categories != categories ||
+        cakeImageDetail.tags != tags {
+      return true
+    }
+    
+    return false
   }
 }
