0d14276cdc1aaf1728c9392aad6188ef33da2310
Wallaby <avocado34.131@gmail.com>
2024-07-02 15:53:06 +0900

    feat(iOS_App) 사장님 기능 coordinator 수정

--- Projects/iOS_App/Sources/Application/Coordinator/ProfileTabCoordinator.swift
+++ Projects/iOS_App/Sources/Application/Coordinator/ProfileTabCoordinator.swift
@@ -9,29 +9,77 @@
 import SwiftUI
 import Router
 
-import FeatureUser
-
 import DomainBusinessOwner
+import DomainCakeShop
+
+import FeatureUser
+import FeatureCakeShopAdmin
+
+import DIContainer
 
 struct ProfileTabCoordinator: View {
   
+  // MARK: - Properties
+  
   @StateObject private var router = Router()
+  private let diContainer = DIContainer.shared.container
+  
+  
+  // MARK: - Views
   
   var body: some View {
     NavigationStack(path: $router.navPath) {
+      let _ = Self._printChanges()
       UserCoordinator()
         .navigationDestination(for: PublicUserDestination.self) { destination in
           switch destination {
-          case .editCakeImage:
-            EmptyView()
-          case .editExternalLink:
-            EmptyView()
-          case .editLocation:
-            EmptyView()
-          case .editShopProfile:
-            EmptyView()
-          case .editWorkingDay:
-            EmptyView()
+          case .editCakeImages(let shopId):
+            let _ = diContainer.register(EditCakeShopImagesViewModel.self) { resolver in
+              let cakeImagesByShopIdUseCase = resolver.resolve(CakeImagesByShopIdUseCase.self)!
+              return EditCakeShopImagesViewModel(shopId: shopId,
+                                                 cakeImagesByShopIdUseCase: cakeImagesByShopIdUseCase)
+            }
+            EditCakeImagesCoordinator()
+              .environmentObject(router)
+            
+          case .editExternalLink(let shopId, let externalLinks):
+            let _ = diContainer.register(EditExternalLinkViewModel.self) { resolver in
+              let editExternalLinkUseCase = resolver.resolve(EditExternalLinkUseCase.self)!
+              return EditExternalLinkViewModel(shopId: shopId,
+                                               editExternalLinkUseCase: editExternalLinkUseCase,
+                                               externalShopLinks: externalLinks)
+            }
+            EditExternalLinkView()
+              .environmentObject(router)
+            
+          case .editLocation(let shopId, let cakeShopLocation):
+            let _ = diContainer.register(EditCakeShopAddressViewModel.self) { resolver in
+              let editShopAddressUseCase = resolver.resolve(EditShopAddressUseCase.self)!
+              return EditCakeShopAddressViewModel(shopId: shopId,
+                                                  cakeShopLocation: cakeShopLocation,
+                                                  editShopAddressUseCase: editShopAddressUseCase)
+            }
+            EditCakeShopAddressView()
+              .environmentObject(router)
+            
+          case .editShopBasicInfo(let shopDetail):
+            let _ = diContainer.register(EditCakeShopBasicInfoViewModel.self) { resolver in
+              let editShopBasicInfoUseCase = resolver.resolve(EditShopBasicInfoUseCase.self)!
+              return EditCakeShopBasicInfoViewModel(shopDetail: shopDetail, editShopBasicInfoUseCase: editShopBasicInfoUseCase)
+            }
+            EditCakeShopBasicInfoView()
+              .environmentObject(router)
+            
+          case .editWorkingDay(let shopId, let workingDaysWithTime):
+            let _ = diContainer.register(EditWorkingDayViewModel.self) { resolver in
+              let editWorkingDayUseCase = resolver.resolve(EditWorkingDayUseCase.self)!
+              return EditWorkingDayViewModel(shopId: shopId,
+                                             editWorkingDayUseCase: editWorkingDayUseCase,
+                                             workingDaysWithTime: workingDaysWithTime)
+            }
+            EditWorkingDayView()
+              .environmentObject(router)
+            
           default:
             EmptyView()
           }
@@ -40,6 +88,9 @@ struct ProfileTabCoordinator: View {
     .environmentObject(router)
   }
 }
+
+
+// MARK: - Preview
 
 #Preview {
   ProfileTabCoordinator()
