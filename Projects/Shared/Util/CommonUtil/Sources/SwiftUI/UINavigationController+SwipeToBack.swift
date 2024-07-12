5004fea99450ca026fe8de2aaa937432a6797f51
Wallaby <avocado34.131@gmail.com>
2024-07-12 19:22:29 +0900

    fix(*) Swipe to back 주석 처리
    - swipe to back을 이용하여 내비게이션 조작 시 한 번에 여러개의 destination이 추가되는 등의 버그가 발생함
    - 따라서 위의 이슈보다 swipe to back 기능을 비활성화 하는 것이 현재로서는 사용자 경험에 있어서 더 나은 방향이라 생각하여 주석처리 함

--- Projects/Shared/Util/CommonUtil/Sources/SwiftUI/UINavigationController+SwipeToBack.swift
+++ Projects/Shared/Util/CommonUtil/Sources/SwiftUI/UINavigationController+SwipeToBack.swift
@@ -5,16 +5,16 @@
 //  Created by 이승기 on 6/3/24.
 //  Copyright © 2024 cakk. All rights reserved.
 //
-
-import SwiftUI
-
-extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
-  open override func viewDidLoad() {
-    super.viewDidLoad()
-    interactivePopGestureRecognizer?.delegate = self
-  }
-  
-  public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-    return viewControllers.count > 1
-  }
-}
+//
+//import SwiftUI
+//
+//extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+//  open override func viewDidLoad() {
+//    super.viewDidLoad()
+//    interactivePopGestureRecognizer?.delegate = self
+//  }
+//  
+//  public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+//    return viewControllers.count > 1
+//  }
+//}
